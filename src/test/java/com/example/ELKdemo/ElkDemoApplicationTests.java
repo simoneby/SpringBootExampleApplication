40ffc79032be061b4bce5996675aa08e61a4e0cf
unknown <simone.bystadhagen@gmail.com>
2020-06-23 11:38:10 +0200

    first commit

---
+++ src/test/java/com/example/ELKdemo/ElkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ELKdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
