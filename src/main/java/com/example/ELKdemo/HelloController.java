d91f3a2ce9de8c5230c8a2c7eb7c734716c9b67b
unknown <simone.bystadhagen@gmail.com>
2020-07-01 15:16:38 +0200

    add database connection

--- src/main/java/com/example/ELKdemo/HelloController.java
+++ src/main/java/com/example/ELKdemo/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
