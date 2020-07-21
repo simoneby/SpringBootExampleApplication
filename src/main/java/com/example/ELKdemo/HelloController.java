1a09909af3bf221418b9ca5869c8fcda23458c2d
Ola Opheim <Ola.Opheim@klp.no>
2020-07-21 11:07:42 +0200

    bla

--- src/main/java/com/example/ELKdemo/HelloController.java
+++ src/main/java/com/example/ELKdemo/HelloController.java
@@ -3,13 +3,58 @@ package com.example.ELKdemo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
 
 
-    @RequestMapping("/pulse")
+    @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/pulse")
+    public String index2() {
+        HttpGet request = new HttpGet("https://www.google.com/search?q=mkyong");
+
+        // add request headers
+        //request.addHeader("custom-key", "mkyong");
+        //request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+
+            // Get HttpResponse Status
+            System.out.println(response.getStatusLine().toString());
+
+            HttpEntity entity = response.getEntity();
+            Header headers = entity.getContentType();
+            System.out.println(headers);
+
+            if (entity != null) {
+                // return it as a String
+                String result = EntityUtils.toString(entity);
+                return result;
+                System.out.println(result);
+            }
+
+        }
+        return "No entry...";
     }
 
 }
